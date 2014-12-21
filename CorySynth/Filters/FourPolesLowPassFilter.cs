19631a074cc1f938b69063d185afd3ca5328f445
Cory R. King <me@coryking.com>
2014-12-21 14:16:52 -0800

    Checkpoint.  doesn't work but don't want to toss

--- CorySynth/Filters/FourPolesLowPassFilter.cs
+++ CorySynth/Filters/FourPolesLowPassFilter.cs
@@ -11,78 +11,134 @@ namespace CorySynth.Filters
     /// A "Four Poles" low pass filter
     /// </summary>
     /// <remarks>See http://musicalagents.googlecode.com/svn/trunk/ensemble/src/ensemble/audio/dsp/FilterProcessing.java </remarks>
-    public class FourPolesLowPassFilter :ISampleProvider
+    public class FourPolesLowPassFilter : ISampleProvider
     {
         private WaveFormat waveFormat;
         private ISampleProvider source;
 
-        float omega; // peak freq
-      
+        float _frequency; // peak freq
+
 
         public float Frequency
         {
-            get { return omega; }
+            get { return _frequency; }
             set
             {
-                omega = value;
+                _frequency = value;
             }
+        }
+
+
+        private float _resonance;
+
+        public float Resonance
+        {
+            get { return _resonance; }
+            set { _resonance = value; }
         }
 
 
         public FourPolesLowPassFilter(ISampleProvider source)
         {
-            this.source=source;
+            this.source = source;
             waveFormat = source.WaveFormat;
 
             Frequency = 600.0f;
-
+            Resonance = 1.0f; ;
+            inQueue = new Queue<float>(4);
+            outQueue = new Queue<float>(4);
         }
 
-        float in_1 = 0;
-        float in_2 = 0;
-        float out_1 = 0;
-        float out_2 = 0;
-            
+        float c;
+
+        float a1;
+        float a2;
+        float a3;
+        float b1;
+        float b2;
+
+        /// <summary>
+        ///  rename
+        /// </summary>
+
+        private void SetParams()
+        {
+
+            c = (float)(1.0f / Math.Tan(Math.PI * Frequency / waveFormat.SampleRate));
+            //c = 1.0f / (float)Math.Tan(Math.PI * Frequency / waveFormat.SampleRate); // *0.957f;
+            a1 = 1.0f / (1.0f + Resonance * c + c * c);
+            a2 = 2 * a1;
+            a3 = a1;
+            b1 = 2.0f * (1.0f - c * c) * a1;
+            b2 = (1.0f - Resonance * c + c * c) * a1;
+        }
+        private Queue<float> inQueue;
+        private Queue<float> outQueue;
+
+        float in1, in2;
+        float out1, out2;
+
+        public float Process(float input)
+        {
+            float output = (a1 * input) +
+                             (a2 * in1) +
+                             (a3 * in2) -
+                             (b1 * out1) -
+                             (b2 * out2);
+
+            in2 = in1;
+            in1 = input;
+
+            out2 = out1;
+            out1 = output;
+
+            //Console.WriteLine(input + ", " + output);
+
+            return output;
+        }
 
         public int Read(float[] buffer, int offset, int sampleCount)
         {
+            //var sourceBuffer = new float[sampleCount];
+            int samplesRead = source.Read(buffer, 0, sampleCount);
 
-            int samplesRead = source.Read(buffer, offset, sampleCount);
-            float r=1;
-            float c = 1.0f / (float)Math.Tan(Math.PI * Frequency / waveFormat.SampleRate) * 0.957f;
-		      float a1 = 1.0f / ( 1.0f + r * c + c * c);
-		      float a2 = 2* a1;
-		      float a3 = a1;
-		      float b1 = 2.0f * ( 1.0f - c*c) * a1;
-		      float b2 = ( 1.0f - r * c + c * c) * a1;
 
-            
-            var queue = new Queue<float>();
-            queue.Enqueue(buffer[0+offset]);
-            queue.Enqueue(buffer[1 + offset]);
-            queue.Enqueue(buffer[2 + offset]);
+            //inQueue.Enqueue(sourceBuffer[0]);
+            //inQueue.Enqueue(sourceBuffer[1]);
+            //inQueue.Enqueue(sourceBuffer[2]);
 
-		      buffer[0+offset]= a1 * queue.ElementAt(0) + a2 * in_1 + a3 * in_2 - b1*out_1 - b2*out_2;
-		      buffer[1+offset]= a1 * queue.ElementAt(1) + a2 * queue.ElementAt(0) + a3 * in_1 - b1*buffer[0+offset] - b2*out_1;
-		      queue.Dequeue();
-		      //double max = 0;
-		      //System.out.println("in-1" + in_1 + "in-2" + in_2 + "out-1" + out_1 + "out-2" + out_2); 
-		      for (int i = 2; i < samplesRead; i++) {
-                  queue.Enqueue(buffer[i+offset]);
-		    	  buffer[i+offset] = a1 * queue.ElementAt(0) + a2 * queue.ElementAt(1) + a3 * queue.ElementAt(2) - b1*buffer[i-1 + offset] - b2*buffer[i-2 + offset];
-		    	  //if(out[i]< max)max = out[i];
-		        queue.Dequeue();
-              }
-		      //System.out.println("sampleRate = "+ sampleRate +  "f = "+ freq + "  Coefs: a1 " + a1  +" a2"+ a2 +" a3"+ a3 +" b1 "+ b1 +" b2 " +b2);
-		      //System.out.println("out-1 = " + out[count-1]);
-		      //System.out.println("out-2 = " + out[count-2]);
-		      //System.out.println("MAX = " + max);
-		      
-		      in_1 = queue.Dequeue();
-		      in_2 = queue.Dequeue();
-		      out_1 = buffer[samplesRead-1 + offset];
-		      out_2 = buffer[samplesRead-2 + offset];
-            
+            //buffer[0 + offset] = a1 * inQueue.ElementAt(0) + a2 * in_1 + a3 * in_2 - b1 * out_1 - b2 * out_2;
+            //buffer[1 + offset] = a1 * inQueue.ElementAt(1) + a2 * inQueue.ElementAt(0) + a3 * in_1 - b1 * buffer[0 + offset] - b2 * out_1;
+            //inQueue.Dequeue();
+            //double max = 0;
+            //System.out.println("in-1" + in_1 + "in-2" + in_2 + "out-1" + out_1 + "out-2" + out_2); 
+            for (int i = 0; i < samplesRead; i++)
+            {
+                inQueue.Enqueue(buffer[i + offset]);
+                buffer[i + offset] = 
+                    (a1 * inQueue.ElementAtOrDefault(3))
+                    + (a2 * inQueue.ElementAtOrDefault(2))
+                    + (a3 * inQueue.ElementAtOrDefault(1))
+                    - (b1 * outQueue.ElementAtOrDefault(1))
+                    - (b2 * outQueue.ElementAtOrDefault(0));
+                outQueue.Enqueue(buffer[i + offset]);
+                //if(out[i]< max)max = out[i];
+                if(inQueue.Count > 4)
+                    inQueue.Dequeue();
+
+                if (outQueue.Count > 2)
+                    outQueue.Dequeue();
+            }
+            //System.out.println("sampleRate = "+ sampleRate +  "f = "+ freq + "  Coefs: a1 " + a1  +" a2"+ a2 +" a3"+ a3 +" b1 "+ b1 +" b2 " +b2);
+            //System.out.println("out-1 = " + out[count-1]);
+            //System.out.println("out-2 = " + out[count-2]);
+            //System.out.println("MAX = " + max);
+
+            //in_1 = inQueue.Dequeue();
+            //in_2 = inQueue.Dequeue();
+            //out_1 = buffer[samplesRead - 1 + offset];
+            //out_2 = buffer[samplesRead - 2 + offset];
+
             return samplesRead;
 
         }
