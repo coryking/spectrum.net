53d20c019778a34b19a184bcb08ae93e8fcc34a7
Cory R. King <me@coryking.com>
2014-12-30 20:36:01 -0800

    get it to run on the surface!

---
+++ SignalGenerator/Sounds/SignalGeneratedSound.cs
@@ -0,0 +1,50 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using CorySignalGenerator.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorySignalGenerator.Sounds
+{
+    public class SignalGeneretedSound: ISoundModel
+    {
+        public SignalGeneretedSound(WaveFormat waveFormat)
+        {
+            WaveFormat = waveFormat;
+        }
+
+        public SignalGeneratorType Type { get; set; }
+
+        public float ReleaseSeconds { get; set; }
+        public float AttackSeconds { get; set; }
+
+        public WaveFormat WaveFormat
+        {
+            get;
+            private set;
+        }
+
+        public ISampleProvider GetProvider(float frequency, int velocity)
+        {
+            return new SignalGeneratorNote(WaveFormat)
+            {
+                Frequency = frequency,
+                Velocity = velocity,
+                Type = Type,
+                ReleaseSeconds = ReleaseSeconds,
+                AttackSeconds = AttackSeconds
+            };
+        }
+
+
+        public void InitSamples()
+        {
+            // Don't do anything.  we generate stuff on the fly
+            return;
+        }
+
+    }
+}
---
+++ UnitTests/CircularBufferTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CorySignalGenerator.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CircularBufferTests
+    {
+        [TestMethod]
+        public void TestArrayIndex()
+        {
+            int size=3;
+
+
+            CircularBuffer<int> circularBuffer = new CircularBuffer<int>(size);
+
+            for (int n = 0; n < size; n++)
+            {
+                circularBuffer[n]=n;
+            }
+            Assert.AreEqual(0, circularBuffer[0]);
+            Assert.AreEqual(1, circularBuffer[1]);
+            Assert.AreEqual(2, circularBuffer[2]);
+
+            circularBuffer.Advance(1);
+            Assert.AreEqual(0, circularBuffer[1]);
+            Assert.AreEqual(1, circularBuffer[2]);
+
+            circularBuffer[0]=10;
+            Assert.AreEqual(10, circularBuffer[0]);
+            Assert.AreEqual(0, circularBuffer[1]);
+            Assert.AreEqual(1, circularBuffer[2]);
+
+
+            circularBuffer.Advance(1);
+            Assert.AreEqual(10, circularBuffer[1]);
+            Assert.AreEqual(0, circularBuffer[2]);
+        }
+
+    }
+}
