53eed9b86968694344c4c749abcef90b1ba44050
Cory R. King <me@coryking.com>
2015-01-08 11:27:58 -0800

    Add a sample provider that just calls a delegate

---
+++ SignalGenerator/SampleProviders/FuncSampleProvider.cs
@@ -0,0 +1,43 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorySignalGenerator.SampleProviders
+{
+    public delegate int SampleProviderRead(float[] buffer, int offset, int count);
+
+    /// <summary>
+    /// A sample provider that calls <see cref="Function"/>
+    /// </summary>
+    public class FuncSampleProvider :ISampleProvider
+    {
+        public FuncSampleProvider(WaveFormat format,SampleProviderRead readFunction)
+        {
+            WaveFormat = format;
+            Function = readFunction;
+
+        }
+
+        public SampleProviderRead Function { get; set; }
+
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            Debug.Assert(Function != null);
+            if (Function == null)
+                return 0;
+
+            return Function.Invoke(buffer, offset, count);
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get;
+            private set;
+        }
+    }
+}
--- UnitTests/ReadRateChangeTests.cs
+++ UnitTests/ReadRateChangeTests.cs
@@ -5,31 +5,7 @@ using CorySignalGenerator.SampleProviders;
 
 namespace UnitTests
 {
-    public class DummyProvider : ISampleProvider
-    {
-        Func<float[], int, int, int> _reader;
-        WaveFormat _format;
-        public DummyProvider(Func<float[], int,int, int> reader, WaveFormat format=null)
-        {
-            if (format == null)
-                _format = WaveFormat.CreateIeeeFloatWaveFormat(44100, 1);
-            else
-                _format = format;
-
-            _reader = reader;
-        }
-
-        public int Read(float[] buffer, int offset, int count)
-        {
-            return _reader.Invoke(buffer, offset, count);
-        }
-
-        public WaveFormat WaveFormat
-        {
-            get { return _format; }
-        }
-    }
-
+   
     [TestClass]
     public class ReadRateChangeTests
     {
@@ -71,18 +47,20 @@ namespace UnitTests
         private static void ReadRateTest(float[][] sourceSample, float[] expectedDestBuffer, float[] destBuffer, int expectedFramesRead, int sourceStride, int destStride)
         {
             var callCount = 0;
-            var source = new DummyProvider((buffer, offset, count) =>
-            {
-                if (callCount >= sourceSample.Length)
-                    return 0;
+            var source = new FuncSampleProvider(
+                WaveFormat.CreateIeeeFloatWaveFormat(44100, 1),
+                (buffer, offset, count) =>
+                {
+                    if (callCount >= sourceSample.Length)
+                        return 0;
 
-                var sample = sourceSample[callCount];
-                Assert.AreEqual(sourceStride, count);
-                Assert.AreEqual(0, offset);
-                Array.Copy(sample, buffer, sample.Length);
-                callCount++;
-                return sample.Length;
-            });
+                    var sample = sourceSample[callCount];
+                    Assert.AreEqual(sourceStride, count);
+                    Assert.AreEqual(0, offset);
+                    Array.Copy(sample, buffer, sample.Length);
+                    callCount++;
+                    return sample.Length;
+                });
 
             // The read function should have been called once ber element in sourceSample
             var provider = new ReadRateChangeProvider(source, sourceStride);
