7893ba7b99f97c3dcf33e368c1c0603319226643
Cory R. King <me@coryking.com>
2015-02-17 09:44:34 -0800

    The UI now works thanks to a converter to/from bytes.
    Makes me wonder why I'm using bytes as a data type

---
+++ CorySynthUI/CorySynthUI.Shared/Converters/ByteTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+using System.ComponentModel;
+namespace CorySynthUI.Converters
+{
+    public class ByteTypeConverter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var orig = (byte)value;
+            return System.Convert.ToDouble(orig);
+                
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var orig = (double)value;
+            return System.Convert.ToByte(orig);
+        }
+    }
+}
